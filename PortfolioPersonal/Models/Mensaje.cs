18dbc27dda8eaee2e2a1d4f49d7436ca415a664d
fidux700 <fernandomgoni@gmail.com>
2022-10-05 23:49:11 -0300

    primer subida

---
+++ PortfolioPersonal/Models/Mensaje.cs
@@ -0,0 +1,22 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioPersonal.Models
+{
+	public class Mensaje
+	{
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
+        [MaxLength(100, ErrorMessage = "La cantidad de caracteres permitida es hasta 100")]
+
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo Email es obligatorio")]
+        [EmailAddress(ErrorMessage = "Debes ingresar un email de contacto")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo del mensaje es obligatorio")]
+        [MaxLength(200, ErrorMessage = "La cantidad de caracteres permitida es hasta 200")]
+        public string TuMensaje { get; set; }
+
+	}
+}
